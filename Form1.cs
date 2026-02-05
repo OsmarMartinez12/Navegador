ab9cce53eae7a3068c24f3ca17b67cea89f8f897
Ahiez <Ahiez@AHIEZER>
2026-02-05 09:09:28 -0600

    Guardar historial

--- Form1.cs
+++ Form1.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using static System.Net.WebRequestMethods;
+
 
 namespace Navegador
 {
@@ -15,8 +17,23 @@ namespace Navegador
     {
         public Form1()
         {
+
             InitializeComponent();
             this.Resize += new System.EventHandler(this.Form_Resize);
+            FileStream stream = new FileStream("Historial.txt", FileMode.Open, FileAccess.Read);
+            StreamReader reader = new StreamReader(stream);
+
+
+
+            while (reader.Peek() > -1)
+            //Esta linea envía el texto leído a un control richTextBox, se puede cambiar para que
+            //lo muestre en otro control por ejemplo un combobox
+            {
+                comboBox1.Items.Add(reader.ReadLine());
+
+            }
+            //Cerrar el archivo, esta linea es importante porque sino despues de correr varias veces el programa daría error de que el archivo quedó abierto muchas veces. Entonces es necesario cerrarlo despues de terminar de leerlo.
+            reader.Close();
         }
 
         private void Form_Resize(object sender, EventArgs e)
@@ -24,6 +41,18 @@ namespace Navegador
             webView21.Size = this.ClientSize - new System.Drawing.Size(webView21.Location);
             button1.Left = this.ClientSize.Width - button1.Width;
             comboBox1.Width = button1.Left - comboBox1.Left;
+        }
+        private void Guardar(string fileName, string texto)
+        {
+            //Abrir el archivo: Write sobreescribe el archivo, Append agrega los datos al final del archivo
+            FileStream stream = new FileStream(fileName, FileMode.Append, FileAccess.Write);
+            //Crear un objeto para escribir el archivo
+            StreamWriter writer = new StreamWriter(stream);
+            //Usar el objeto para escribir al archivo, WriteLine, escribe linea por linea
+            //Write escribe todo en la misma linea. En este ejemplo se hará un dato por cada línea
+            writer.WriteLine(texto);
+            //Cerrar el archivo
+            writer.Close();
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -35,6 +64,7 @@ namespace Navegador
         private void button1_Click(object sender, EventArgs e)
         {
             string Url = comboBox1.Text;
+            
 
 
             if (!(Url.Contains("https://") || (Url.Contains("http://"))))
@@ -48,6 +78,7 @@ namespace Navegador
             }
 
             webView21.Source = new Uri(Url);
+            Guardar(@"Historial.txt", Url);
         }
 
         
